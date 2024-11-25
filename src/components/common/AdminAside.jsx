8de45a526fd1b5b4be13807f8629dfc160607d19
eziquexx <waftyann@gmail.com>
2024-11-25 19:18:59 +0900

    react-bootstrap 의존성 설치, admin aside 메뉴 수정

--- src/components/common/AdminAside.jsx
+++ src/components/common/AdminAside.jsx
@@ -8,112 +8,106 @@ export default function AdminAside() {
       <Accordion flush>
         <Accordion.Item eventKey="0">
           <Accordion.Header>
+            <p className="adminAsideMenu">
+              <span className="material-symbols-outlined icons">group</span>
+              <span>회원관리</span>
+            </p>
+          </Accordion.Header>
+          <Accordion.Body>
             <ButtonEx
               id={"member"}
               url={"/admin/member"}
-              className={"adminAsideMenu"}
+              className={"adminAsideMenu adminAsideSubMenu"}
             >
               <p>
-                <span className="material-symbols-outlined icons">group</span>
-                <span>회원관리</span>
+                <span className="material-symbols-outlined icons">
+                  check_indeterminate_small
+                </span>
+                <span>회원전체목록</span>
               </p>
             </ButtonEx>
-          </Accordion.Header>
-          <Accordion.Body>
-            <p>
-              <span className="material-symbols-outlined icons">
-                check_indeterminate_small
-              </span>
-              <span>회원전체목록</span>
-            </p>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
           <Accordion.Header>
+            <p className="adminAsideMenu">
+              <span className="material-symbols-outlined icons">
+                account_box
+              </span>
+              <span>직원관리</span>
+            </p>
+          </Accordion.Header>
+          <Accordion.Body>
             <ButtonEx
               id={"staff"}
               url={"/admin/staff"}
-              className={"adminAsideMenu"}
+              className={"adminAsideMenu adminAsideSubMenu"}
             >
               <p>
                 <span className="material-symbols-outlined icons">
-                  account_box
+                  check_indeterminate_small
                 </span>
-                <span>직원관리</span>
+                <span>직원전체목록</span>
               </p>
             </ButtonEx>
-          </Accordion.Header>
-          <Accordion.Body>
-            <p>
-              <span className="material-symbols-outlined icons">
-                check_indeterminate_small
-              </span>
-              <span>직원전체목록</span>
-            </p>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
           <Accordion.Header>
+            <p className="adminAsideMenu">
+              <span className="material-symbols-outlined icons">bed</span>
+              <span>객실관리</span>
+            </p>
+          </Accordion.Header>
+          <Accordion.Body>
             <ButtonEx
               id={"room"}
               url={"/admin/room"}
-              className={"adminAsideMenu"}
+              className={"adminAsideMenu adminAsideSubMenu"}
             >
               <p>
-                <span className="material-symbols-outlined icons">bed</span>
-                <span>객실관리</span>
+                <span className="material-symbols-outlined icons">
+                  check_indeterminate_small
+                </span>
+                <span>객실전체목록</span>
               </p>
             </ButtonEx>
-          </Accordion.Header>
-          <Accordion.Body>
-            <p>
-              <span className="material-symbols-outlined icons">
-                check_indeterminate_small
-              </span>
-              <span>객실전체목록</span>
-            </p>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="3">
           <Accordion.Header>
+            <p className="adminAsideMenu">
+              <span className="material-symbols-outlined icons">acute</span>
+              <span>예약관리</span>
+            </p>
+          </Accordion.Header>
+          <Accordion.Body>
             <ButtonEx
               id={"reservation"}
               url={"/admin/reservation"}
-              className={"adminAsideMenu"}
+              className={"adminAsideMenu adminAsideSubMenu"}
             >
               <p>
-                <span className="material-symbols-outlined icons">acute</span>
-                <span>예약관리</span>
+                <span className="material-symbols-outlined icons">
+                  check_indeterminate_small
+                </span>
+                <span>예약전체목록</span>
               </p>
             </ButtonEx>
-          </Accordion.Header>
-          <Accordion.Body>
-            <p>
-              <span className="material-symbols-outlined icons">
-                check_indeterminate_small
-              </span>
-              <span>예약전체목록</span>
-            </p>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="4">
           <Accordion.Header>
+            <p className="adminAsideMenu">
+              <span className="material-symbols-outlined icons">paid</span>
+              <span>결제관리</span>
+            </p>
+          </Accordion.Header>
+          <Accordion.Body>
             <ButtonEx
               id={"paymentsGroup"}
               url={"/admin/payments"}
               // action={fetch('')}
-              className={"adminAsideMenu"}
-            >
-              <p>
-                <span className="material-symbols-outlined icons">paid</span>
-                <span>결제관리</span>
-              </p>
-            </ButtonEx>
-          </Accordion.Header>
-          <Accordion.Body>
-            <ButtonEx
-              id={"payments"}
-              url={"/admin/payments"}
               className={"adminAsideMenu adminAsideSubMenu"}
             >
               <p>
@@ -139,50 +133,50 @@ export default function AdminAside() {
         </Accordion.Item>
         <Accordion.Item eventKey="5">
           <Accordion.Header>
+            <p className="adminAsideMenu">
+              <span className="material-symbols-outlined icons">
+                view_cozy
+              </span>
+              <span>게시판관리</span>
+            </p>
+          </Accordion.Header>
+          <Accordion.Body>
             <ButtonEx
               id={"boards"}
               url={"/admin/boards"}
-              className={"adminAsideMenu"}
+              className={"adminAsideMenu adminAsideSubMenu"}
             >
               <p>
                 <span className="material-symbols-outlined icons">
-                  view_cozy
+                  check_indeterminate_small
                 </span>
-                <span>게시판관리</span>
+                <span>공지사항 게시판</span>
               </p>
             </ButtonEx>
-          </Accordion.Header>
-          <Accordion.Body>
-            <p>
-              <span className="material-symbols-outlined icons">
-                check_indeterminate_small
-              </span>
-              <span>공지사항 게시판</span>
-            </p>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="6">
           <Accordion.Header>
+            <p className="adminAsideMenu">
+              <span className="material-symbols-outlined icons">
+                monitoring
+              </span>
+              <span>통계관리</span>
+            </p>
+          </Accordion.Header>
+          <Accordion.Body>
             <ButtonEx
               id={"statistics"}
               url={"/admin/statistics"}
-              className={"adminAsideMenu"}
+              className={"adminAsideMenu adminAsideSubMenu"}
             >
               <p>
                 <span className="material-symbols-outlined icons">
-                  monitoring
+                  check_indeterminate_small
                 </span>
-                <span>통계관리</span>
+                <span>통계1</span>
               </p>
             </ButtonEx>
-          </Accordion.Header>
-          <Accordion.Body>
-            <p>
-              <span className="material-symbols-outlined icons">
-                check_indeterminate_small
-              </span>
-              <span>통계1</span>
-            </p>
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
