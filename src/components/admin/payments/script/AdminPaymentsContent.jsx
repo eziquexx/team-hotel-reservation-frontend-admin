7190e2d0ac6b583ab8f8dcd13fc74b3035d199be
eziquexx <waftyann@gmail.com>
2024-11-25 19:37:33 +0900

    24.11.25 지은 : 결제관리 UI 디자인 작업중 1

--- src/components/admin/payments/script/AdminPaymentsContent.jsx
+++ src/components/admin/payments/script/AdminPaymentsContent.jsx
@@ -1,9 +1,60 @@
-// AdminMemberContent
+import Table from 'react-bootstrap/Table';
+
+// 24.11.25 지은 [작업중] : UI 디자인만 구현 - no data
 export default function AdminPaymentsContent() {
   return (
     <div>
       <h1>결제관리</h1>
-      <div>결제관리</div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>결제ID</th>
+            <th>예약ID</th>
+            <th>회원ID</th>
+            <th>회원이름</th>
+            <th>결젤방식</th>
+            <th>결제금액</th>
+            <th>결제날짜</th>
+            <th>결제변경날짜</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>존 도우</td>
+            <td>PAYAPL</td>
+            <td>COMPLETED</td>
+            <td>200000.00</td>
+            <td>2024-11-21T12:46:24</td>
+            <td>2024-11-23T13:04:42</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>제인 도우</td>
+            <td>PAYAPL</td>
+            <td>COMPLETED</td>
+            <td>300000.00</td>
+            <td>2024-11-21T12:46:24</td>
+            <td>2024-11-21T12:46:24</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>3</td>
+            <td>3</td>
+            <td>마이크 존슨</td>
+            <td>PAYAPL</td>
+            <td>COMPLETED</td>
+            <td>500000.00</td>
+            <td>2024-11-21T12:46:24</td>
+            <td>2024-11-23T15:27:01</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
