64f176a10bab5f1de064ae668d0b67e52db54149
an <ssungjune0610@gmail.com>
2024-11-29 09:15:59 +0900

    24.11.28 성준 : 집가자

--- src/components/admin/member/script/AdminMemberContent.jsx
+++ src/components/admin/member/script/AdminMemberContent.jsx
@@ -1,9 +1,60 @@
-// AdminMemberContent
-export default function AdminMemberContent() {
-  return (
-    <div>
-      <h1>회원관리</h1>
-      <div>회원관리</div>
-    </div>
-  );
+import Table from 'react-bootstrap/Table';
+
+
+export default function AdminPaymentsContent() {
+    return (
+        <div>
+            <h1>회원관리</h1>
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>결제ID</th>
+                    <th>예약ID</th>
+                    <th>회원ID</th>
+                    <th>회원이름</th>
+                    <th>결젤방식</th>
+                    <th>결제금액</th>
+                    <th>결제날짜</th>
+                    <th>결제변경날짜</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>존 도우</td>
+                    <td>PAYAPL</td>
+                    <td>COMPLETED</td>
+                    <td>200000.00</td>
+                    <td>2024-11-21T12:46:24</td>
+                    <td>2024-11-23T13:04:42</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>2</td>
+                    <td>2</td>
+                    <td>제인 도우</td>
+                    <td>PAYAPL</td>
+                    <td>COMPLETED</td>
+                    <td>300000.00</td>
+                    <td>2024-11-21T12:46:24</td>
+                    <td>2024-11-21T12:46:24</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>3</td>
+                    <td>3</td>
+                    <td>마이크 존슨</td>
+                    <td>PAYAPL</td>
+                    <td>COMPLETED</td>
+                    <td>500000.00</td>
+                    <td>2024-11-21T12:46:24</td>
+                    <td>2024-11-23T15:27:01</td>
+                </tr>
+                </tbody>
+            </Table>
+        </div>
+    );
 }
