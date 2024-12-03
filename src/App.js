d4f0735f494fd806741e81b19e4f486924980b42
jelee <waftyann@gmail.com>
2024-12-03 17:04:10 +0900

    24.12.03 지은 : create-browser-router 적용 및 경로 수정과 header, aside UI 스타일 수정 작업 fin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminPage from "./pages/AdminPage";
+import { RouterProvider } from "react-router-dom";
+import RouterObject from "./util/router";
 
+//24.12.03 지은 [완료] : create-browser-router 적용
 function App() {
-  return (
-    <BrowserRouter basename="/" future={{ v7_startTransition: true }}>
-      <Routes>
-        <Route path="/admin/*" element={<AdminPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={RouterObject} />;
 }
 
 export default App;
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import AdminContents from "../components/common/AdminContents";
 import AdminHeader from "../components/common/AdminHeader";
+import AdminContents from "../components/common/AdminContents";
 
 const AdminContainerStyle = {
   display: "flex",
@@ -8,7 +8,7 @@ const AdminContainerStyle = {
   height: "100vh",
 };
 
-//24.11.25 지은 [완료] : 관리자 페이지 경로 테스트.
+//24.12.03 지은 [완료] : create-browser-router 적용을 하면서 경로 수정
 export default function AdminPage() {
   return (
     <div style={AdminContainerStyle}>
