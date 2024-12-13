e73bf15eb0daaf46159351dd689fe7acc0fe190b
an <ssungjune0610@gmail.com>
2024-12-13 16:29:18 +0900

    24.12.13 성준 : 리다이렉트

--- src/util/router.jsx
+++ src/util/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createBrowserRouter, Navigate, redirect } from "react-router-dom";
 import AdminBoardsContent from "../components/admin/boards/script/AdminBoardsContent";
 import AdminMemberContent from "../components/admin/member/script/AdminMemberContent";
 import AdminPaymentsContent from "../components/admin/payments/script/AdminPaymentsContent";
@@ -8,9 +8,10 @@ import AdminStaffContent from "../components/admin/staff/script/AdminStaffConten
 import AdminStatisticsContent from "../components/admin/statistics/script/AdminStatisticsContent";
 import AdminPage from "../pages/AdminPage";
 import AdminHomeContent from "../components/admin/adminHome/script/AdminHomeContent";
+// import AdminPaymentsList from "../components/admin/payments/script/AdminPaymentsList";
+// import AdminPaymentsPaypalList from "../components/admin/payments/script/AdminPaymentsPaypalList";
 import AdminLoginPage from "../pages/AdminLoginPage";
-
-import PrivateRoute from "../components/common/PrivateRoute";
+import { jwtDecode } from "jwt-decode";
 
 const ErrorPage = () => {
   return (
@@ -28,21 +29,77 @@ export const RouterInfo = [
   },
   {
     path: "/admin",
-    element: (
-        <PrivateRoute>
-          <AdminPage />
-        </PrivateRoute>
-    ),
+    element: <AdminPage />,
     errorElement: <ErrorPage />,
+    loader: () => {
+      const token = document.cookie
+          .split("; ")
+          .find((row) => row.startsWith("JWT="))
+          ?.split("=")[1];
+
+      if (token) {
+        try {
+          const decodedToken = jwtDecode(token);
+          // "ADMIN" 역할 및 토큰 만료 여부 확인
+          if (decodedToken.role === "ADMIN" && Date.now() < decodedToken.exp * 1000) {
+            return decodedToken.userId ? decodedToken.userId : decodedToken.staffUserId;
+          } else {
+            // ADMIN 역할이 아니거나 토큰이 만료된 경우 리디렉션
+            return redirect("/admin/login");
+          }
+        } catch (error) {
+          console.error("Error decoding token:", error);
+          // 토큰 디코딩 오류 발생 시 리디렉션
+          return redirect("/admin/login");
+        }
+      } else {
+        // 토큰이 없으면 리디렉션
+        return redirect("/admin/login");
+      }
+    },
     children: [
-      { index: true, element: <AdminHomeContent /> },
-      { path: "member", element: <AdminMemberContent /> },
-      { path: "staff", element: <AdminStaffContent /> },
-      { path: "room", element: <AdminRoomContent /> },
-      { path: "reservation", element: <AdminReservationContent /> },
-      { path: "payments", element: <AdminPaymentsContent /> },
-      { path: "boards", element: <AdminBoardsContent /> },
-      { path: "statistics", element: <AdminStatisticsContent /> },
+      {
+        index: true,
+        element: <AdminHomeContent />,
+      },
+      {
+        path: "member",
+        element: <AdminMemberContent />,
+      },
+      {
+        path: "staff",
+        element: <AdminStaffContent />,
+      },
+      {
+        path: "room",
+        element: <AdminRoomContent />,
+      },
+      {
+        path: "reservation",
+        element: <AdminReservationContent />,
+      },
+      // {
+        // path: "payments",
+        // element: <AdminPaymentsContent />,
+        // children: [
+        //   {
+        //     path:"",
+        //     element: <AdminPaymentsList />,
+        //   },
+        //   {
+        //     path:"paypal",
+        //     element: <AdminPaymentsPaypalList />,
+        //   }
+        // ]
+      // },
+      {
+        path: "boards",
+        element: <AdminBoardsContent />,
+      },
+      {
+        path: "statistics",
+        element: <AdminStatisticsContent />,
+      },
     ],
   },
   {
