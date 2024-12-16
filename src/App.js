7dac36edc71e31669d8109f47605bee9d26c051d
jelee <waftyann@gmail.com>
2024-12-16 12:37:36 +0900

    24.12.16 지은 :  browserRouter -> CreateBrowserRouter 변경

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdminLoginPage from "./pages/AdminLoginPage";
-import AdminPage from "./pages/AdminPage";
+import { RouterProvider } from "react-router-dom";
+import RouterObject from "./util/router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import AdminLoginPage from "./pages/AdminLoginPage";
+// import AdminPage from "./pages/AdminPage";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<AdminLoginPage />} />
-                <Route path="/admin" element={<AdminPage />} />
-            </Routes>
-        </Router>
-    );
+    return <RouterProvider router={RouterObject} />;
 }
 
 export default App;
